4e6acc378f5e3708036c8ba3d1f474c64e2adb92
Lorran F Lazaro <lorran.lazaro@al.infnet.edu.br>
2024-05-26 01:41:41 -0300

    Entrega Versão final 1.3

--- lib/providers/exerc_provider.dart
+++ lib/providers/exerc_provider.dart
@@ -2,7 +2,13 @@ import 'package:flutter/cupertino.dart';
 import '../models/exercicio.dart';
 
 class ExercProvider extends ChangeNotifier {
-  List<Exercicio>? exercicios = [];
+  List<Exercicio>? exercicios = [
+    Exercicio ("Voador Peitoral", "3 x 12 repetições", 30),
+    Exercicio ("Supino Reto", "3 x 10 repetições", 50),
+    Exercicio ("Biceps Corda", "3 x 10 repetições", 20),
+    Exercicio ("Rosca Biceps Alternada", "3 x 10 repetições", 50),
+    Exercicio ("Remada Apoiada", "3 x 10 repetições", 70),
+  ];
 
   void addExerc(Exercicio exerc) {
     exercicios?.add(exerc);
